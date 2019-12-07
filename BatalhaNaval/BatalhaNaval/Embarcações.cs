abbe57dd987ceae516a298749101fe04d436b4df
Roberty <santosrobertypfs@gmail.com>
2019-12-07 12:11:24 -0300

    Atualização final
    Jogo Finalizado

---
+++ BatalhaNaval/BatalhaNaval/Embarcações.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BatalhaNaval
+{
+    class Embarcações
+    {
+        private String[] end = new String[16];
+
+        public String[] End
+        {
+            get { return end; }
+            set { end = value; }
+        }
+    }
+}
